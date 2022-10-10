e36783e9184afbe701dfd102ad715492f2f25906
will <w.duffy@outlook.com>
2022-10-10 16:11:55 -0500

    added test file

--- problems/05-filter-user-profiles.js
+++ problems/05-filter-user-profiles.js
@@ -21,39 +21,9 @@ filterUserProfiles(facebookUsers, (user) => user.state === "New York"); // ["Mik
 ***********************************************************************/
 
 function filterUserProfiles(users, filter) {
-  // Your code here
-//   let newArr = []
 
-//   users.forEach(user =>{
-//     // console.log(user)
-//     if(filter(user)){
-//       // console.log("NAME", user.name)
-//       newArr.push(user.name)
-//     }
-//   })
-//   return newArr
-// }
-
-let res = [];
-
-  let filtered = users.filter(user => filter(user))
-  for (let i=0; i<filtered.length; i++){
-    let ele = filtered[i];
-    res.push(ele.name)
-  }
-  return res;
 }
 
-
-const facebookUsers = [
-  { name: "John", age: "21", state: "Florida" },
-  { name: "Mary", age: "57", state: "California" },
-  { name: "Judy", age: "47", state: "Texas" },
-  { name: "Mike", age: "32", state: "New York" }
-];
-
-console.log(filterUserProfiles(facebookUsers, (user) => user.age > 30)); // ["Mary", "Judy", "Mike"]
-console.log(filterUserProfiles(facebookUsers, (user) => user.state === "New York")); // ["Mike"]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
